5065cf2f17638c623efe7f0ca3ea4e806dae7622
David Silva <davidsilvav6@gmail.com>
2023-11-27 22:41:52 -0500

    General Commit

---
+++ js/index.js
@@ -0,0 +1,78 @@
+ //console.log("Hellow World")
+
+
+ //variables antes de ecmascript 6 
+ 
+
+ //var variableVieja =  10;  //no usar var es solo para la explicación 
+
+
+
+ //la forma nueva de hacer variables es => let y constant 
+//let es que un dato puede cambiar dentro de nuestro codigo 
+
+ //let primerNumero = 1;
+ //console.log(primerNumero);
+ //primerNumero = 2;
+
+// console.log(primerNumero)
+
+/*const es una variable que tiene un dato constante y se suelen escribir en mayuscula con guion bajo cuando le colocamos dos nombres se trata 
+ de poner en mayuscula pero no hay una regla fija*/
+
+ //const CONSTANTE_NUMERO = 4;
+
+
+ //console.log(CONSTANTE_NUMERO);
+
+
+
+ //let nombre;
+ //nombre = "davidSilva";
+ //console.log (nombre);
+
+
+
+//let numero = 1;
+
+//let string = "cadena de caracteres";
+
+//let bolean = true; //false 
+
+
+//let suma = 5 + 3;
+//console.log(suma)
+
+
+
+//let numeroUno = 85;
+//let numeroDos = 145;
+
+//let resultado = numeroUno * numeroDos;
+//console.log(resultado);
+
+
+//let nombre = "David";
+//let apellido = "Silva";
+//let vacio = " ";
+
+
+//let nombreApellido = nombre + vacio + apellido;
+//console.log("Hola mundo mi nombre es: " + //nombre + " " + apellido)
+
+ //prompt y alert 
+
+//let nombre = prompt ("Ingresa tu p nombre ");
+
+//let numero1 = parseInt(prompt("Ingresa tu numero"));
+//let numero2 = parseInt(prompt("Ingresa otro numero"));
+
+//let resultado = numero1 + numero2;
+//alert (resultado)
+
+//let numero
+
+//OPERADORES
+
+
+
